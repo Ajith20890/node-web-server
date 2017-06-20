14b3b8f66871af7b25c3cb12b339073fd018142c
AjithKrishnaAithal <ajith.aithal20890@gmail.com>
2017-06-21 00:47:45 +0530

    setup start script and heroku port and 2nd commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const hbs=require('hbs');
 
 var app= express();
 
+const port =process.env.PORT||3000;
+
 hbs.registerPartials(__dirname+'/views/partials');
 
 app.set('view engine','hbs');
@@ -57,6 +59,6 @@ app.get('/bad',(req,res)=>{
 })
 });
 
-app.listen(3000,()=>{
-  console.log('server is up at 3000');
+app.listen(port,()=>{
+  console.log(`server is up at ${port}.`);
 });
